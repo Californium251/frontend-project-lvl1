15b9c66e748fc6c0219408a4bb54def1869da75d
Maxim Kurepov <maximkurepov@Maxims-MacBook-Pro.local>
2022-05-08 20:39:32 +0300

    Вернул назад все игры, потому что тесты не проходили. Не понимаю, почему тогда в условии сказано, что игры должны быть в отдельной папке games, если тесты проверяют их наличие в папке bin

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 import userName from '../src/cli.js';
-import * as get from '../bin/get.js';
-import * as answer from '../bin/answer.js';
-import ask from '../bin/ask.js';
-import findCorrectAnswer from '../bin/findCorrectAnswer.js';
+import * as get from './get.js';
+import * as answer from './answer.js';
+import ask from './ask.js';
+import findCorrectAnswer from './findCorrectAnswer.js';
 
 const brainEven = () => {
   const gameName = 'brainEven';
