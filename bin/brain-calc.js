a4fa56185f7bea8608f77c60b5db3c964a752384
Maxim Kurepov <maximkurepov@Maxims-MacBook-Pro.local>
2022-05-10 03:05:57 +0300

    linter mistakes fixed

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -3,4 +3,4 @@
 import { condition, getQuestion, findCorrectAnswer } from '../src/games/calc.js';
 import startGame from '../src/index.js';
 
-startGame(condition, getQuestion, findCorrectAnswer)
+startGame(condition, getQuestion, findCorrectAnswer);
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const cli = (condition) => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log(condition);
-    return userName;
-}
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(condition);
+  return userName;
+};
 
 export default cli;
